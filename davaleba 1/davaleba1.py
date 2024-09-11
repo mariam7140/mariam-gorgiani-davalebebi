25b7b7fd6b69dde76daac4d03259fb080fff66ab
mariam7140 <147436785+mariam7140@users.noreply.github.com>
2024-09-11 23:14:13 +0400

    old

--- davaleba 1/davaleba1.py
+++
@@ -1,6 +0,0 @@
-# programam unda shegvayvaninos saxeli, gvari asaki
-# outcome: {sheyvanili saxeli, gvari}, თქვენ ხართ {შეყვანილი წლოვანიბა}
-
-name = input("What is your name? " )
-age = input("what is you age? " )
-print(name, "you are", age)
--- davaleba 1/davaleba4.py
+++
@@ -1,5 +0,0 @@
-# შეიყვანე ცელსიუსი
-
-c = input("what is the temperature celsius: ")
-f = (c * 9 / 5) + 32
-print("this temperature in fahrenheit is:", f)
