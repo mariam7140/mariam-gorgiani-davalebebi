2dbb39e91433bb092628856fd0ed139105b6b5ec
mariam7140 <147436785+mariam7140@users.noreply.github.com>
2024-09-18 21:35:47 +0400

    comment

--- davaleba 1/davaleba4.py
+++ davaleba 1/davaleba4.py
@@ -1,5 +1,5 @@
 # შეიყვანე ცელსიუსი
 
-c = input("what is the temperature celsius: ")
-f = (c * 9 / 5) + 32
+c = int(input("what is the temperature celsius: "))
+f = c * 9 / 5 + 32
 print("this temperature in fahrenheit is:", f)
